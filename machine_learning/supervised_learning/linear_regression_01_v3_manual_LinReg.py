b5f8200cd2d298d72ba80f172e2eecd0dd9f5c14
Simon R. Böhmer <Simon@U-TVWMJHWY-1542.fritz.box>
2025-10-20 17:23:38 +0200

    solved shadow name issue with y_pred

--- machine_learning/supervised_learning/linear_regression_01_v3_manual_LinReg.py
+++ machine_learning/supervised_learning/linear_regression_01_v3_manual_LinReg.py
@@ -36,8 +36,8 @@ class LinearRegression:
 # model error (SSR): Sum of squared differences between actual (y_true) and predicted (y_pred) values
 # total variation (SST): Sum of squared differences between actual values and their mean
 # Intuition: R^2 (0 to 1) shows how much better the model predicts the actual data compared to using just the mean
-def r2_score(y_true, y_pred):
-    ssr = np.sum((y_true - y_pred) ** 2)  # model error (sum of squared residuals)
+def r2_score(y_true, y_pred_arg):
+    ssr = np.sum((y_true - y_pred_arg) ** 2)  # model error (sum of squared residuals)
     sst = np.sum((y_true - np.mean(y_true)) ** 2)  # total variation in y_true
     return 1 - ssr / sst
 
